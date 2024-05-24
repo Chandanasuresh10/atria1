509057c871002f6fc86e574d3161705a4a6c6a14
chandana <chandana@LAPTOP-3ISIB5JL>
2024-05-24 09:49:00 +0530

    first commit

--- Atria/src/com/day1/SecondProgram.java
+++ Atria/src/com/day1/SecondProgram.java
@@ -4,19 +4,21 @@ public class SecondProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String name,usn,branch;
+String name,usn,branch,address;
  float marks;
  Scanner sc= new Scanner(System.in);
- System.out.println("enter the studnt name,usn,branch,marks");
+ System.out.println("enter the studnt name,usn,branch,marks,address");
  name=sc.nextLine();
  usn=sc.nextLine();
  branch=sc.nextLine();
  marks=sc.nextFloat();
+ address=sc.nextLine();
  System.out.println("The details are : ");
  System.out.println("the name "+name);
  System.out.println("the usn" +usn);
  System.out.println("the branch"+branch);
  System.out.println("the marks" + marks);
+ System.out.println("the address" + address);
 	}
 
 }
